4380cde300507e024b6d58eefd34e03a46053c11
joshdarling <joshua.a.darling@gmail.com>
2022-02-28 17:57:51 -0700

    all excercises complete

---
+++ app.js
@@ -0,0 +1,19 @@
+//let and const Exercise
+
+
+//ES2015 Global Constants
+
+const PI = 3.14;
+PI = 42; // you cannot reassign a const variable!!!
+
+
+
+
+
+//Quiz
+
+//What is the difference between var and let? -> You can reassign and redeclare with var, but you can not redeclare using the let keyword, let is scope based var is function based, var can be added to the dom windwow, let cannot be ran before a decleration
+//What is the difference between var and const? -> const variable cannot be reassigned or redelcared but can mutate arrays and objects, var can be redeclared and reassign, var can be hoisted
+//What is the difference between let and const? -> let can be reassigned even outside of arrays and objects as long as let is not redeclared, const cannot, both are scope based
+ //What is hoisting? -> var variable is compiled are ran before they are declared it is hoisted to the top of the scope it is declared in. Function declarations are also hoisted and can be invoked “before” they are defined in a codebase.
+
